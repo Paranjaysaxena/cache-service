c2776a19ccfb7fcb42b60bd5175c6faf9f59b3e1
Paranjaysaxena <paranjayasaxena@gmail.com>
2021-08-03 10:03:28 +0530

    update client

--- client/main.go
+++ client/main.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Println("Hello Client.. ")
 
 	opts := grpc.WithInsecure()
-	cc, err := grpc.Dial("localhost:50051", opts)
+	cc, err := grpc.Dial("localhost:8000", opts)
 
 	if err != nil {
 		panic(err)
@@ -23,13 +23,18 @@ func main() {
 
 	client := gen.NewAddServiceClient(cc)
 
-	request := &gen.Request{Key: "Name", Value: []byte("Paranjaya")}
+	request1 := &gen.Request{Key: "Name", Value: []byte("Paranjaya Saxena")}
+	request2 := &gen.Request{Key: "Age", Value: []byte("22")}
 
-	setResponse, _ := client.Set(context.Background(), request)
+	setResponse1, _ := client.Set(context.Background(), request1)
+	setResponse2, _ := client.Set(context.Background(), request2)
 
-	getResponse, _ := client.Get(context.Background(), request)
+	getResponse1, _ := client.Get(context.Background(), request1)
+	getResponse2, _ := client.Get(context.Background(), request2)
 
-	fmt.Println("Set Function Response", setResponse)
-	fmt.Println("Get Function Response", getResponse)
+	fmt.Println("Set Function Response1", setResponse1)
+	fmt.Println("Set Function Response2", setResponse2)
+	fmt.Println("Get Function Response1", getResponse1)
+	fmt.Println("Get Function Response2", getResponse2)
 
 }
