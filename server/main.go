8dedafff95d580517e9ce9d50f472885f67096f0
Paranjaysaxena <paranjayasaxena@gmail.com>
2021-08-03 10:03:55 +0530

    update server to use redis

--- server/main.go
+++ server/main.go
@@ -1,37 +1,51 @@
 package main
 
 import (
+	"cache-service/database"
 	gen "cache-service/proto"
 	"context"
 	"fmt"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 )
 
-type server struct {
-}
+type server struct{}
+
+var db database.Database
 
 func main() {
-	listener, err := net.Listen("tcp", "0.0.0.0:50051")
+	listener, err := net.Listen("tcp", "0.0.0.0:8000")
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Server is listening at port 50051")
+	fmt.Println("Server is listening at port 8000")
 
 	srv := grpc.NewServer()
+
+	databaseImplementation := os.Args[1]
+	db, err = database.Factory(databaseImplementation)
+	if err != nil {
+		panic(err)
+	}
+
 	gen.RegisterAddServiceServer(srv, &server{})
 
 	srv.Serve(listener)
 
 }
 
-func (*server) Set(ctx context.Context, request *gen.Request) (*gen.Response, error) {
-	return &gen.Response{Value: request.Value}, nil
+func (s *server) Set(ctx context.Context, request *gen.Request) (*gen.Response, error) {
+	key := "Paranjaya:" + request.GetKey()
+	value, err := db.Set(key, request.GetValue())
+	return &gen.Response{Value: value}, err
 }
 
-func (*server) Get(ctx context.Context, request *gen.Request) (*gen.Response, error) {
-	return &gen.Response{Value: request.Value}, nil
+func (s *server) Get(ctx context.Context, request *gen.Request) (*gen.Response, error) {
+	key := "Paranjaya:" + request.GetKey()
+	value, err := db.Get(key)
+	return &gen.Response{Value: value}, err
 }
